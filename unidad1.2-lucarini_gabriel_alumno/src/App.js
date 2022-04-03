990a505f5aff5e2a6b4e179cdbf7ab4ce25f463c
gabilucarini <gabilucarini18@gmail.com>
2022-04-03 16:38:48 -0300

    task1_finish

--- unidad1.2-lucarini_gabriel_alumno/src/App.js
+++ unidad1.2-lucarini_gabriel_alumno/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Title from "./components/Title.js";
+import Body from "./components/Body.js";
+
+const site = {
+  pageTitle: 'Unidad 1.2 - Práctica',
+  pageBody: 'Implementación de árbol de componentes y propiedades en React.'
+}
+
 
 function App() {
+
   return (
-    <div className="App">
+    <div>
+      <Title pageTitle={site.pageTitle}/>
+      <Body pageBody={site.pageBody}/>
     </div>
   );
 }
