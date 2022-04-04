6986ad9c548c0ebd47a7c104a9e4877755a36272
Mauro <mauropaccioretti@hotmail.com>
2022-04-04 20:27:02 -0300

    refactorizado children

--- unidad_1_2/unidad1.2-paccioretti_mauro/src/App.js
+++ unidad_1_2/unidad1.2-paccioretti_mauro/src/App.js
@@ -1,6 +1,3 @@
-// import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
 import Page from "./components/Page";
 
 function App() {
@@ -9,20 +6,7 @@ function App() {
     pageBody: "Implementación de árbol de componentes y propiedades en React.",
   };
 
-  return (
-    <div className="App">
-      <Page>
-        <Header pageTitle={site.pageTitle} />
-        <Main
-          pageBody={site.pageBody}
-          enlace1={"https://www.google.com"}
-          enlace2={"https://github.com/mauropaccioretti"}
-        >
-          Frase enviada como children desde app a main
-        </Main>
-      </Page>
-    </div>
-  );
+  return <Page site={site} />;
 }
 
 export default App;
--- unidad_1_2/unidad1.2-paccioretti_mauro/src/components/Page.js
+++ unidad_1_2/unidad1.2-paccioretti_mauro/src/components/Page.js
@@ -1,5 +1,19 @@
-const Page = ({ children }) => {
-  return <div>{children}</div>;
+import Header from "./Header";
+import Main from "./Main";
+
+const Page = ({ site }) => {
+  return (
+    <div>
+      <Header pageTitle={site.pageTitle} />
+      <Main
+        pageBody={site.pageBody}
+        enlace1={"https://www.google.com"}
+        enlace2={"https://github.com/mauropaccioretti"}
+      >
+        Frase enviada como children desde page a main
+      </Main>
+    </div>
+  );
 };
 
 export default Page;
