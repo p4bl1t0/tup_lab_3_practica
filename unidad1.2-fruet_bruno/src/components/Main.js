bdb908040c97a7eabec7eb339fa6af95ed3e3daf
brunofruet <brunofruet17@gmail.com>
2022-04-04 23:41:24 -0300

    rev 1

--- unidad1.2-fruet_bruno/src/components/Main.js
+++ unidad1.2-fruet_bruno/src/components/Main.js
@@ -4,18 +4,15 @@ import Body from "./Body";
 const Main = ({ body }) => {
   return (
     <div>
-      <nav>
-        <h3>MENU</h3>
-      </nav>
-      <ul>
-        <li>
-          <a href="https://www.infobae.com/">Infobae</a>
-        </li>
-        <li>
-          <a href="https://www.rosario3.com/">Rosario 3</a>
-        </li>
-      </ul>
       <Body pBody={body} />
+      <h3>MENU</h3>
+      <ul>
+        <a href="https://www.infobae.com/">Infobae</a>
+      </ul>
+      <ul>
+        <a href="https://www.rosario3.com/">Rosario 3</a>
+      </ul>
+      <p>La pelota no se mancha</p>
     </div>
   );
 };
