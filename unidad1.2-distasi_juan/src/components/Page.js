369b269ad491108e2b5af98283c08dcb5ed5dffd
juanods <juanods1@gmail.com>
2022-04-06 16:04:22 -0300

    Remove unecessary imports

--- unidad1.2-distasi_juan/src/components/Page.js
+++ unidad1.2-distasi_juan/src/components/Page.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Page = ({ children }) => {
   return <div>{children}</div>;
 };
--- unidad1.2-distasi_juan/src/components/Title.js
+++ unidad1.2-distasi_juan/src/components/Title.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Title = ({ pageTitle }) => {
   return <h1>{pageTitle}</h1>;
 };
