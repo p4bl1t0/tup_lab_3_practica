3b9646b9c2b355111dd3c2c2ae6e100a7478cb0f
EzeMartino <ezequielrmartino@gmail.com>
2022-04-06 21:15:03 -0300

    Delete unused imports

--- unidad_1_2/unidad1.2-martino-ezequiel/src/components/Page.js
+++ unidad_1_2/unidad1.2-martino-ezequiel/src/components/Page.js
@@ -1,13 +1,7 @@
 import React from "react";
-import Header from "./Header.js";
-import Main from "./Main";
 
 const Page = ({ children }) => {
-  return (
-    <div>
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default Page;
