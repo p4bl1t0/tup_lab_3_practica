1bdd5e6c078e1606121035b12b9597208178a330
juan-ds <juanods1@gmail.com>
2022-04-18 15:07:50 -0300

    Realizar práctica de useState

--- unidad_2_state/unidad_2_managing_state/src/App.js
+++ unidad_2_state/unidad_2_managing_state/src/App.js
@@ -1,20 +1,45 @@
-import './App.css';
+import "./App.css";
+
+import LogicGates from "./components/LogicGates";
+import { useState } from "react";
 
 function App() {
+  const [newInput, setNewInput] = useState(0);
+  const [checkboxInput, setCheckboxInput] = useState(0);
+
+  const inputHandler = (e) => {
+    let inputValue = e.target.value;
+    if (inputValue <= 1 && inputValue >= 0) {
+      setNewInput(inputValue);
+    } else setNewInput(0);
+  };
+
+  const checkboxHandler = (e) => {
+    if (e.target.checked) {
+      setCheckboxInput(1);
+    } else setCheckboxInput(0);
+  };
+
   return (
     <div className="App">
       <div>
         <label for="input1">Entrada 1:</label>
-        <input type="number" id="input1" />
+        <input
+          type="number"
+          id="input1"
+          min="0"
+          max="1"
+          onChange={inputHandler}
+        />
       </div>
       <div>
-        <label><input type="checkbox" /> Entrada 2</label>
+        <label>
+          <input type="checkbox" onChange={checkboxHandler} /> Entrada 2
+        </label>
       </div>
-        <div>
-        <label for="selectGate">Compuerta lógica:</label>
-        <select id="selectGate"></select>
+      <div>
+        <LogicGates newInput={newInput} checkboxInput={checkboxInput} />
       </div>
-        <div><span>Salida: { /* aca iria el resultado*/ }</span></div>
     </div>
   );
 }
---
+++ unidad_2_state/unidad_2_managing_state/src/components/LogicGates.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const LogicGates = ({ newInput, checkboxInput }) => {
+  const [selectInput, setSelectInput] = useState("");
+  const selectChangeHandler = (e) => {
+    setSelectInput(e.target.value);
+  };
+  const calculateResult = (gate, a, b) => {
+    a = Number(a);
+    b = Number(b);
+    switch (gate) {
+      case "and":
+        return Number(a && b);
+      case "or":
+        return Number(a || b);
+      case "xor":
+        return Number(a !== b);
+      case "nand":
+        return Number(!(a && b));
+      case "nor":
+        return Number(!(a || b));
+      default:
+        return "Seleccione una puerta lógica";
+    }
+  };
+
+  return (
+    <div>
+      <label for="selectGate">Compuerta lógica:</label>
+      <select id="selectGate" onChange={selectChangeHandler}>
+        <option default hidden></option>
+        <option value="or">OR</option>
+        <option value="and">AND</option>
+        <option value="nand">NAND</option>
+        <option value="nor">NOR</option>
+        <option value="xor">XOR</option>
+      </select>
+      <div>
+        <span>
+          Salida: {calculateResult(selectInput, newInput, checkboxInput)}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default LogicGates;
