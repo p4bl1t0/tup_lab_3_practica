d3b840cba40114c077dfa2146c9553414d6a7cef
Feli-Regis <felipeandresregis@gmail.com>
2022-04-19 15:06:58 -0300

    Practice unit 2 completed

--- unidad_2_state/unidad_2_managing_state/src/App.js
+++ unidad_2_state/unidad_2_managing_state/src/App.js
@@ -1,20 +1,71 @@
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
 
 function App() {
+  const [newInput1, setNewInput1] = useState(Math.round(Math.random()));
+  const [checked1, setChecked1] = useState("");
+  const [result, setResult] = useState("");
+
+  const changeInput = (event) => {
+    setNewInput1(event.target.value === "1" ? 1 : 0);
+  };
+
+  const ChangeCheckbox = () => {
+    setChecked1(checked1 === 1 ? 0 : 1);
+  };
+
+  const changeSelectGate = (event) => {
+    switch (event.target.value) {
+      case "OR":
+        setResult(newInput1 || checked1 ? 1 : 0);
+        break;
+      case "AND":
+        setResult(newInput1 && checked1 ? 1 : 0);
+        break;
+      case "NOR":
+        setResult(newInput1 || checked1 ? 0 : 1);
+        break;
+      case "NAND":
+        setResult(newInput1 && checked1 ? 0 : 1);
+        break;
+      case "XOR":
+        setResult(newInput1 !== checked1 ? 1 : 0);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="App">
       <div>
         <label for="input1">Entrada 1:</label>
-        <input type="number" id="input1" />
+        <input
+          type="number"
+          id="input1"
+          value={newInput1}
+          onChange={changeInput}
+        />
       </div>
       <div>
-        <label><input type="checkbox" /> Entrada 2</label>
+        <label>
+          <input type="checkbox" onChange={ChangeCheckbox} /> Entrada 2
+        </label>
       </div>
-        <div>
+      <div>
         <label for="selectGate">Compuerta lógica:</label>
-        <select id="selectGate"></select>
+        <select id="selectGate" onChange={changeSelectGate}>
+          <option>OR</option>
+          <option>AND</option>
+          <option>NAND</option>
+          <option>NOR</option>
+          <option>XOR</option>
+        </select>
       </div>
-        <div><span>Salida: { /* aca iria el resultado*/ }</span></div>
+      <div>
+        <span>Salida: {result}</span>
+      </div>
     </div>
   );
 }
