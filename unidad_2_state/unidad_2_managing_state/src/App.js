5ff6fc171dbaabd37c379d8a68de24bcc5a9bd6a
lucapec <88390994+lucapec@users.noreply.github.com>
2022-04-21 14:15:22 -0300

    Tasks State - Luca Pecorelli
    Task 1-2 Done! Taks 3 -4 - 5 incomplete

--- unidad_2_state/unidad_2_managing_state/src/App.js
+++ unidad_2_state/unidad_2_managing_state/src/App.js
@@ -1,18 +1,63 @@
 import './App.css';
 
+import './LogicGate';
+
+import {useState} from "react";
+
 function App() {
+
+  const [input1, setInput1] = useState(Math.round(Math.random())); //seteo el valor aleatoriamente entre 1 y 0
+  const changeInputValue = (event) => {
+    //console.log(event.target.value);
+    setInput1(event.target.value == 1 ? 1 : 0);
+  }
+
+  const [checkBox, setCheckBox] = useState(0);
+  const changeCheckBox = (event) => {
+    //console.log(event.target.value);
+    setCheckBox(event.target.value = 1 );
+  }
+
+  const [selectValue, setSelectValue] = useState('');
+  const evaluateGate = (event) =>{
+    //console.log(event.target.value);
+    setSelectValue(event.target.value);
+  }
+
+  const calculateGate = () => {
+    switch(selectValue){
+      case "and":
+        return (input1 && checkBox);
+      case "or":
+        return
+      case "nand":
+        return
+      case "nor":
+        return
+      case "xor":
+        return
+    }
+  }
+
   return (
     <div className="App">
       <div>
         <label for="input1">Entrada 1:</label>
-        <input type="number" id="input1" />
+        <input type="number" id="input1" value={input1} onChange = {changeInputValue} />
       </div>
       <div>
-        <label><input type="checkbox" /> Entrada 2</label>
+        <label><input type="checkbox" value={checkBox} onChange = {changeCheckBox}/> Entrada 2</label>
       </div>
-        <div>
+      <div>
         <label for="selectGate">Compuerta lógica:</label>
-        <select id="selectGate"></select>
+        <select id="selectGate" value={selectValue} onChange = {evaluateGate}>
+        <option>Seleccione</option>
+        <option value={"and"}>AND</option>
+        <option value={"or"}>OR</option>
+        <option value={"nand"}>NAND</option>
+        <option value={"nor"}>NOR</option>
+        <option value={"xor"}>XOR</option>
+        </select>
       </div>
         <div><span>Salida: { /* aca iria el resultado*/ }</span></div>
     </div>
---
+++ unidad_2_state/unidad_2_managing_state/src/LogicGate.js
@@ -0,0 +1,11 @@
+import { useState } from "react"
+   
+
+const evaluateGate = () => {
+    <div>
+        <label for="selectGate">Compuerta lógica:</label>
+        <select id="selectGate"></select>
+      </div>
+}
+
+export default evaluateGate;
