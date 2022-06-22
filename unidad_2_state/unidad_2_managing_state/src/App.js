5ea7e64960b2637217f8ae663fb61c3581098cd7
Mateopc <mateogd012@gmail.com>
2022-06-22 16:56:59 -0300

    Ej 3 2.2 Ok

--- unidad_2_state/unidad_2_managing_state/src/App.js
+++ unidad_2_state/unidad_2_managing_state/src/App.js
@@ -1,20 +1,56 @@
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
+
+import LogicGate from "./components/LogicGate";
 
 function App() {
+  const [inputValue, setInputValue] = useState("1");
+  const [checkBoxValue, setCheckBoxValue] = useState(0);
+  
+  const inputHandler = (elem) => {
+    const intValue = parseInt(elem.target.value);
+    if (intValue < 0 || intValue > 1) {
+      setInputValue(0);
+    } else {
+      setInputValue(1);
+    }
+  };
+
+  const checkboxHandler = (elem) => {
+    if (elem.target.checked) {
+      setCheckBoxValue(1);
+    } else {
+      setCheckBoxValue(0);
+    }
+  };
+
   return (
     <div className="App">
       <div>
         <label for="input1">Entrada 1:</label>
-        <input type="number" id="input1" />
+        <input
+          type="number"
+          id="input1"
+          value={inputValue}
+          onChange={inputHandler}
+        />
       </div>
       <div>
-        <label><input type="checkbox" /> Entrada 2</label>
+        <label>
+          <input
+            type="checkbox"
+            onChange={checkboxHandler}
+            checked={checkBoxValue}
+          />
+          Entrada 2
+        </label>
       </div>
-        <div>
-        <label for="selectGate">Compuerta lógica:</label>
-        <select id="selectGate"></select>
-      </div>
-        <div><span>Salida: { /* aca iria el resultado*/ }</span></div>
+      <LogicGate
+        inputValue={inputValue}
+        checkBoxValue={checkBoxValue}
+      />
+
     </div>
   );
 }
---
+++ unidad_2_state/unidad_2_managing_state/src/components/LogicGate.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { useState } from "react";
+
+const LogicGate = ({
+  inputValue,
+  checkBoxValue,
+}) => {
+
+  const [selectValue, setSelectValue] = useState("OR");
+  const selectHandler = (elem) => {
+    setSelectValue(elem.target.value);
+    ;
+  };
+
+  const setResult = () => {
+    if (selectValue === "OR") {
+      return inputValue || checkBoxValue?"TRUE":"FALSE";
+    } else if (selectValue === "AND") {
+      return inputValue && checkBoxValue?"TRUE":"FALSE";
+    } else if (selectValue === "NAND") {
+      return (1 - (inputValue && checkBoxValue))?"TRUE":"FALSE";
+    } else if (selectValue === "NOR") {
+      return(1 - (inputValue || checkBoxValue))?"TRUE":"FALSE";
+    } else if (selectValue === "XOR") {
+      return inputValue ^ checkBoxValue?"TRUE":"FALSE";
+    }
+  };
+
+  return (
+    <div>
+      <label for="selectGate">Compuerta lógica:</label>
+      <select id="selectGate" value={selectValue} onChange={selectHandler}>
+        <option value="OR">OR</option>
+        <option value="AND">AND</option>
+        <option value="NAND">NAND</option>
+        <option value="NOR">NOR</option>
+        <option value="XOR">XOR</option>
+      </select>
+      <div>
+        <span>Salida: {setResult()}</span>
+      </div>
+    </div>
+  );
+};
+
+export default LogicGate;
