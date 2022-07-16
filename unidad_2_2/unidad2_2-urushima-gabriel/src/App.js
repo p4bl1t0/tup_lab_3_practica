d21766d75bec23194bd0c80471b1116b26addd83
giurushima <giurushima@hotmail.com>
2022-07-16 12:43:30 -0300

    Ejercicio unidad2_2 terminado. Falta ultimos puntos de practica 2_state

--- unidad_2_2/unidad2_2-urushima-gabriel/src/App.js
+++ unidad_2_2/unidad2_2-urushima-gabriel/src/App.js
@@ -1,33 +1,50 @@
-import logo from "./logo.svg";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [input, setInputText] = useState("");
+  const [input, setInput] = useState("");
   const [array, setArray] = useState([]);
+  const [edit, setEdit] = useState();
+  const [editBool, setEditBool] = useState(false);
 
   const handleInput = (e) => {
-    setInputText(e.target.value);
+    setInput(e.target.value);
   };
 
   const handleAdd = () => {
-    input !== "" ? setArray([...array, input]) : console.log("Vacio");
-    setInputText("");
+    if (input !== "") {
+      if (!editBool) {
+        setArray([...array, input]);
+      } else {
+        const listFilter = array.filter((var1, var2) => var2 !== edit);
+        setArray([...listFilter, input]);
+      }
+      setEditBool(false);
+      setInput("");
+    } else {
+      console.log("Campo vacio");
+    }
   };
 
-  const removeInput = (indexItem) => {
-    setArray((array) => array.filter((array, index) => index !== indexItem));
+  const removeInput = (index) => {
+    setArray((array) => array.filter((var1, var2) => var2 !== index));
   };
 
-  const editInput = {
-    
+  const editInput = (index) => {
+    setEditBool(true);
+    setEdit(index);
   };
 
   return (
     <>
       <div>
-        <input type="text" value={input} onChange={handleInput} />
-        <button onClick={handleAdd}>Enviar</button>
+        <input
+          type="text"
+          placeholder="Ingrese texto o numeros"
+          value={input}
+          onChange={handleInput}
+        />
+        <button onClick={handleAdd}>{editBool ? "Editar" : "Agregar"}</button>
       </div>
       <div>
         {array.map((element, index) => (
@@ -35,7 +52,8 @@ function App() {
             {Number(element) ? (
               Number(element % 2) === 0 ? (
                 <p>
-                  Par<button onClick={() => removeInput(index)}>Borrar</button>
+                  Par
+                  <button onClick={() => removeInput(index)}>Borrar</button>
                 </p>
               ) : (
                 <p>
@@ -47,35 +65,13 @@ function App() {
               <div>
                 <input type="text" value={element} />
                 <button onClick={() => removeInput(index)}>Borrar</button>
-                <button onChange={() => editInput}>Editar</button>
+                <button onClick={() => editInput(index)}>Editar</button>
               </div>
             )}
-            {}
           </p>
         ))}
       </div>
     </>
   );
 }
-// const [input, setInput] = useState("");
-// const [array, setArray] = useState([]);
-
-// const changeArray = (e) => {
-//   setArray(e.target.value);
-// };
-
-// const handleAdd = () => {
-//   input !== "" ? setArray([...array, input]) : console.log("Vacio");
-// };
-
-// return (
-//   <form>
-//     <div>
-//       <label htmlFor="texto">Ingrese texto</label>
-//       <input type="text" value={input} onChange={changeArray} />
-//       <button onClick={handleAdd}>Enviar</button>
-//     </div>
-//   </form>
-// );
-// }
 export default App;
