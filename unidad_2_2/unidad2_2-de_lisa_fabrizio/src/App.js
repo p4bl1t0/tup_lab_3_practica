7512faa1736057e9a0f49aee0f38b501edeb0227
fabriziodelisa <fabrizio.de.lisa@gmail.com>
2022-07-17 10:36:40 -0300

    tarea 1.1 lista

--- unidad_2_2/unidad2_2-de_lisa_fabrizio/src/App.js
+++ unidad_2_2/unidad2_2-de_lisa_fabrizio/src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputString, setInputString] = useState([]);
+  const [textValue, setTextValue] = useState("");
+
+  console.log("cadena ", inputString);
+
+  const buttonHandler = () => {
+    const aux = [...inputString.concat(textValue)] ;
+      setInputString(aux);
+      setTextValue("");
+  };
+
+  const inputTextHandler = (e) => {
+    setTextValue(e.target.value)
+  }
+
   return (
     <div className="App">
-
+        <input
+          type="text"
+          max="100"
+          value={textValue}
+          onChange={inputTextHandler}
+        />
+        <button onClick={buttonHandler}>agregar</button>
     </div>
   );
 }
