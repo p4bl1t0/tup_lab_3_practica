8c7f456e236525fb1dc22051cbeaea55107d3949
ddewde <martinaguirre80@gmail.com>
2022-07-24 05:22:23 -0300

    Tarea 1 Hecha

--- unidad_2_2/unidad2_2-aguirre_martin/src/App.js
+++ unidad_2_2/unidad2_2-aguirre_martin/src/App.js
@@ -1,9 +1,51 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [array, setArray] =  useState([]);
+  const handleInputText = (evento) => {
+    setInputText(evento.target.value);
+  }
+  const handleAdd = () => {
+    inputText !== "" ? setArray([...array, inputText]) : console.log("empty");
+    setInputText("");
+  }
+
+  const removeElement = (index) => {
+    const updatedElements = array.filter(
+      (elem, i) => i !== index
+    );
+    setArray([...updatedElements]);
+  }; 
+
+
+
   return (
     <div className="App">
-        <h1>Template Ready</h1>      
+        <input type="text" value={inputText} onChange={handleInputText} />
+        <button onClick={handleAdd}>Enviar</button>
+        <div>
+          {array.map((elem, index) => {
+            let number = Number(elem);
+            return (
+            <p key={index}>
+              {number ? ( 
+              (number % 2 === 0) ? (
+                <p>{elem} es par <button onClick={removeElement}>Borrar</button></p>                
+              ) : (
+                <p>{elem} es impar <button onClick={removeElement}>Borrar</button></p>
+              )
+              ) : (        
+                <div>
+                  <input type="text" value={elem} />                
+                  <button onClick={removeElement}>Borrar</button>
+                </div>          
+              )}
+            </p>
+            );
+        })}
+        </div>
     </div>
   );
 }
