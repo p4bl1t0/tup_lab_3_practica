4856ebc0bcbc5b7ec2c9caeb34bef113981823c5
“ddewde“ <martinaguirre80@gmail.com>
2022-07-26 16:39:37 -0300

    tarea 2 unidad state_terminada

--- unidad_2_state/unidad_2_managing_state/src/App.js
+++ unidad_2_state/unidad_2_managing_state/src/App.js
@@ -5,12 +5,35 @@ function App() {
 
   const numeroAleatorio = () => Math.round(Math.random());
   const [input1, setInput1] = useState(numeroAleatorio());
+  const [checkbox, setCheckbox] = useState(0);
+  const [select, setSelect] = useState(0);
 
   const handleInput = (event) => {
     Number(event.target.value) === 1
       ? setInput1(Number(event.target.value))
       : setInput1(0);
   };
+
+  const handleCheckbox = () => {
+    checkbox ? setCheckbox(0) : setCheckbox(1);
+  };
+
+  const handleSelect = (event) => {
+    switch (event.target.value) {
+      case "and":
+        return input1 && checkbox ? setSelect(1) : setSelect(0);
+      case "nand":
+        return !input1 && !checkbox ? setSelect(1) : setSelect(0);
+      case "or":
+        return input1 || checkbox ? setSelect(1) : setSelect(0);
+      case "nor":
+        return !input1 || !checkbox ? setSelect(1) : setSelect(0);
+      case "xor":
+        return input1 ^ checkbox ? setSelect(1) : setSelect(0);
+      default:
+        return "Vuelva a ingresar";
+    }
+  }  
 
 
   return (
@@ -22,12 +45,24 @@ function App() {
     </div>
       </div>
       <div>
-        <label><input type="checkbox" /> Entrada 2</label>
+      <label><input type="checkbox" onChange={handleCheckbox} />
+        Entrada 2
+      </label>
       </div>
-        <div>
-        <label for="selectGate">Compuerta lógica:</label>
-        <select id="selectGate"></select>
-      </div>
+
+      <div>
+      <label htmlFor="selectGate">Compuerta lógica:</label>
+      <select id="selectGate" onChange={handleSelect} defaultValue={"DEFAULT"}>
+        <option value={"DEFAULT"} disabled>
+          Seleccion opcion
+        </option>
+        <option value="and">And x</option>
+        <option value="nand">Nand</option>
+        <option value="or">Or +</option>
+        <option value="nor">Nor</option>
+        <option value="xor">Xor</option>
+      </select>
+    </div>
         <div><span>Salida: { /* aca iria el resultado*/ }</span></div>
     </div>
   );
