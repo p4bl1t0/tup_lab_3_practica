e22f41d9f58edacd99d6ee5824de8595181b7948
“ddewde“ <martinaguirre80@gmail.com>
2022-07-26 16:10:30 -0300

    tarea 1 unidad state2_terminada

--- unidad_2_state/unidad_2_managing_state/src/App.js
+++ unidad_2_state/unidad_2_managing_state/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const numeroAleatorio = () => Math.round(Math.random());
+  const [input1, setInput1] = useState(numeroAleatorio());
+
+  const handleInput = (event) => {
+    Number(event.target.value) === 1
+      ? setInput1(Number(event.target.value))
+      : setInput1(0);
+  };
+
+
   return (
     <div className="App">
       <div>
-        <label for="input1">Entrada 1:</label>
-        <input type="number" id="input1" />
+      <div>
+      <label htmlFor="input1">Entrada 1:</label>
+      <input type="number" value={input1} id="input1" onChange={handleInput} />
+    </div>
       </div>
       <div>
         <label><input type="checkbox" /> Entrada 2</label>
